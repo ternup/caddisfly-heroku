5a9a92c54d8c28b31ed2fcf91ada1c8c92fed70a
Arun Kumar <arun@arun-ternup>
2013-04-02 00:42:15 +0530

    corrected database url

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['HEROKU_POSTGRESQL_PURPLE_URL']
 db = SQLAlchemy(app)
 
 @app.route('/')
